6f9728edd5606156c090e2e7ca358269fb82070a
iagoLGitHub <arnajenkins86@gmail.com>
2024-02-08 16:26:30 +0100

    modify:Control de los EditText y su ventana de error

--- app/src/main/java/com/example/olimpic/controller/Competitor_add.java
+++ app/src/main/java/com/example/olimpic/controller/Competitor_add.java
@@ -2,15 +2,22 @@ package com.example.olimpic.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
 import com.example.olimpic.R;
 import com.example.olimpic.databinding.ActivityCompetitorAddBinding;
+import com.example.olimpic.model.Competitor;
+
 import java.util.Arrays;
 import java.util.List;
 
-public class Competitor_add extends AppCompatActivity {
+public class Competitor_add extends AppCompatActivity implements View.OnClickListener{
 
     ActivityCompetitorAddBinding binding;
     @Override
@@ -24,6 +31,70 @@ public class Competitor_add extends AppCompatActivity {
 
         ArrayAdapter<String>adapter=new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,categorias);
         binding.spinner.setAdapter(adapter);
+        binding.addBtn.setOnClickListener(this);
+        binding.cancelBtn.setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==binding.addBtn.getId()){
+            if(createCompetitor()){
+                System.out.println(true);
+            }
+
+            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        }else if(view.getId()==binding.cancelBtn.getId()){
+            finish();
+        }
+    }
+
+    /**
+     * Crea Competidor, queda implementar bbdd
+     * @return
+     */
+    private boolean createCompetitor(){
+
+        if(controlEditText()){
+            Competitor newCompetitor=
+                    new Competitor(binding.editName.getText().toString(),
+                                   binding.editLastName.getText().toString(),
+                                   binding.editAka.getText().toString(),
+                                   (String) binding.spinner.getSelectedItem());
+
+            return true;
+        }
+
+       return false;
+    }
+
+    private Boolean controlEditText(){
+        if((binding.editName.getText().toString().isEmpty())||
+            binding.editAka.getText().toString().isEmpty()||
+            binding.editAka.getText().toString().isEmpty()){
+            showErrorDialog();
+            return false;
+
+        }else{
+            return true;
+        }
+    }
+
+    private void showErrorDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String title="Error";
+        String message="Es necesario que cubras todos los campos";
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
 }
--- app/src/main/java/com/example/olimpic/model/Competitor.java
+++ app/src/main/java/com/example/olimpic/model/Competitor.java
@@ -8,4 +8,20 @@ public class Competitor {
     private String aka;
     private String Category;
 
+    public Competitor(String name, String lastName, String aka, String category) {
+        this.name = name;
+        this.lastName = lastName;
+        this.aka = aka;
+        Category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Competitor{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", aka='" + aka + '\'' +
+                ", Category='" + Category + '\'' +
+                '}';
+    }
 }
